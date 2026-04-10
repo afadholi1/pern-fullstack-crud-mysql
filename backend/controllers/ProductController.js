991fe458b62ef270e306e7e763cbb9bd680dfce3
afadholi1 <afadholi1@gmail.com>
2026-04-10 16:15:44 +0700

    feat: add product controller and database table schema

---
+++ backend/controllers/ProductController.js
@@ -0,0 +1,64 @@
+import db from "../config/database.js";
+
+// 1. Get All Products
+export const getProducts = async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT * FROM products");
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// 2. Get Product By ID
+export const getProductById = async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT * FROM products WHERE id = ?", [
+      req.params.id,
+    ]);
+    if (rows.length === 0)
+      return res.status(404).json({ message: "Data tidak ditemukan" });
+    res.json(rows[0]);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// 3. Create Product
+export const saveProduct = async (req, res) => {
+  const { name, price } = req.body;
+  try {
+    await db.query("INSERT INTO products (name, price) VALUES (?, ?)", [
+      name,
+      price,
+    ]);
+    res.status(201).json({ message: "Product Created Successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// 4. Update Product
+export const updateProduct = async (req, res) => {
+  const { name, price } = req.body;
+  try {
+    await db.query("UPDATE products SET name = ?, price = ? WHERE id = ?", [
+      name,
+      price,
+      req.params.id,
+    ]);
+    res.json({ message: "Product Updated Successfully" });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+// 5. Delete Product
+export const deleteProduct = async (req, res) => {
+    try {
+        await db.query("DELETE FROM products WHERE id = ?", [req.params.id]);
+        res.json({ message: "Product Deleted Successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
